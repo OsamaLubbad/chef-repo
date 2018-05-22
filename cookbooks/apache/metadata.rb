79216b43810ed20d93a9f465a0f478ae26da0d29
osama <osama.lubbad@progressoft.com>
2018-05-22 17:11:01 +0000

    added basic apache cookbook

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'apache'
+maintainer       'osama'
+maintainer_email 'osama.lubbad@progressoft.com'
+license          'All rights reserved'
+description      'Installs/Configures apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
